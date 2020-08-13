0ec920dcade481384733d1e937fa796e7a647d5f
pro100Sardor <sardorbek.juraqulov@gmail.com>
2020-08-14 00:09:43 +0500

    initial commit

---
+++ server/data.js
@@ -0,0 +1,14 @@
+const users = [
+  {id: 1, username: 'harun', firstName: 'Horun', lastName: 'Najimov', gender: 'Erkak', age: 21, status: 'businessman'},
+  {id: 2, username: 'najmudden', firstName: 'Najmiddin', lastName: 'Najimov', gender: 'Erkak', age: 19, status: 'student'},
+  {id: 3, username: 'suleiman', firstName: 'Sulaymon', lastName: 'Najimov', gender: 'Erkak', age: 16, status: 'schoolboy'},
+  {id: 4, username: 'yahyo', firstName: 'Yahyo', lastName: 'Najimov', gender: 'Erkak', age: 14, status: 'schoolboy'},
+  {id: 5, username: 'hafsa', firstName: 'Hafsa', lastName: 'Najimova', gender: 'Ayol', age: 12, status: 'schoolgirl'},
+  {id: 6, username: 'Muhammadxon', firstName: 'Muhammadxon', lastName: 'Najimov', gender: 'Erkak', age: 25, status: 'software inginer, teacher'},
+  {id: 7, username: 'Muhammadjavohir', firstName: 'Muhammadjavohir', lastName: 'Suratov', gender: 'Erkak', age: 26, status: 'software inginer, teacher'},
+  {id: 8, username: 'MuhammadHusayn', firstName: 'MuhammadHusayn', lastName: 'Olimjonov', gender: 'Erkak', age: 23, status: 'software inginer, teacher'},
+  {id: 9, username: 'ali', firstName: 'Ali', lastName: 'Zairov', gender: 'Erkak', age: 25, status: 'software inginer, teacher'},
+  {id: 10, username: 'Rasul', firstName: 'Rasul', lastName: 'Ismoilov', gender: 'Erkak', age: 20, status: 'software inginer'},
+  {id: 11, username: 'qodir', firstName: 'Qodir', lastName: 'Bozorov', gender: 'Erkak', age: 17, status: 'software inginer'},
+  {id: 12, username: 'abdulatifxon', firstName: 'Abdulatifxon', lastName: 'Anvarxonov', gender: 'Erkak', age: 20, status: 'software inginer'},
+]
